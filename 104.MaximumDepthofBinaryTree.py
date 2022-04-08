d9421cecac2e9e81e2ad4a6622fde97bdcff35b9
Bartosz Piechnik <76113844+bartoszpiechnik25@users.noreply.github.com>
2022-04-08 17:04:36 +0200

    Maximum Tree Depth

---
+++ 104.MaximumDepthofBinaryTree.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None: return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return 1 + left if left > right else 1 + right
