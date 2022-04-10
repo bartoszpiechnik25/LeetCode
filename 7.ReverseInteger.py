8aa15e1700b3089b46ac84d1b1828f9a8d01c158
Bartosz Piechnik <76113844+bartoszpiechnik25@users.noreply.github.com>
2022-04-10 10:51:33 +0200

    Reverse Integer

--- 7.ReverseInteger.py
+++ 7.ReverseInteger.py
@@ -3,7 +3,4 @@ class Solution:
         number = int(str(abs(x))[::-1])
         if (number > pow(2,31)-1):
             return 0
-        else:
-            if x < 0:
-                return -1*number
-            return number
+        return -1*number if x < 0 else number
