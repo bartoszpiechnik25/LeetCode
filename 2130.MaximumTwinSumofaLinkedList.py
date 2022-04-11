995ad901bdf1e4e81dd91e7536838e8a69a97876
Bartosz Piechnik <76113844+bartoszpiechnik25@users.noreply.github.com>
2022-04-11 13:53:06 +0200

    Create 2130.MaximumTwinSumofaLinkedList.py

---
+++ 2130.MaximumTwinSumofaLinkedList.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+        n = len(stack)
+        return max([stack[i] + stack[n-1-i] for i in range(n//2)])
