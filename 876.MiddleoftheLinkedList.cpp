da54feab24169f19b1e6db135f3608aa3b9b3cbe
Bartosz Piechnik <76113844+bartoszpiechnik25@users.noreply.github.com>
2022-12-21 16:00:37 +0100

    Update 876.MiddleoftheLinkedList.cpp

--- 876.MiddleoftheLinkedList.cpp
+++ 876.MiddleoftheLinkedList.cpp
@@ -18,3 +18,15 @@ public:
         return node;
     }
 };
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head, *fast = head;
+        while ( fast && fast->next ) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
